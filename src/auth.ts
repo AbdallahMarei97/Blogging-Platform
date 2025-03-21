0ef5f0653dd09afc4a970f520d2e613dae417771
AbdallahMarei97 <abdullahmari@gmail.com>
2025-03-21 03:49:00 +0300

    trigger CI/CD github action

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,12 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { getUserByEmail } from "./actions/user";
-// Your own logic for dealing with plaintext password strings; be careful!
-// import { saltAndHashPassword } from "@/lib/utils"
 import { authConfig } from "@/auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
       credentials: {
         email: {},
         password: {},
@@ -20,8 +16,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           credentials as { email: string; password: string }
         );
         if ("error" in user) {
-          // No user found, so this is their first attempt to login
-          // Optionally, this is also the place you could do a user registration
           throw new Error("Invalid credentials.");
         }
         return user;
